9061b202270b7c5fd3f1a065425da9b4e69bafde
Matheus Amim Martins de Almeida <124313377+Darks-049@users.noreply.github.com>
2026-03-27 20:52:14 -0300

    Delete Project TCC directory

--- Project TCC/js/home.js
+++
@@ -1,7 +0,0 @@
-
-    //Está merda vai levar o usuário até uma parte da página com um scroll mais sua suave. Resumindo é uma animação
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: 'smooth'
-  });
-}
